c3bcf1d5c277ec86a7c094c802070e7f048868b0
Kent Hua <kenthua@google.com>
2021-01-07 02:09:20 +0000

    fixed autoinstall file, cleaned up url, added isc tftp configs

--- scripts/rpi-pxe.sh
+++ scripts/rpi-pxe.sh
@@ -41,9 +41,12 @@ dhcp-option=6,8.8.8.8,192.168.1.1
 dhcp-option=66,10.10.0.1
 dhcp-option=67,pxelinux.0
 log-queries
-log-facility=/var/log/dnsmasq.log" | sudo tee -a /etc/dnsmasq.conf
+log-facility=/var/log/dnsmasq.log
+dhcp-host=aa:bb:cc:dd:ee:ff,10.10.0.11
+dhcp-host=aa:bb:cc:ff:dd:ee,10.10.0.12
+" | sudo tee -a /etc/dnsmasq.conf
 
-#https://askubuntu.com/questions/1235723/automated-20-04-server-installation-using-pxe-and-live-server-image`
+#https://askubuntu.com/questions/1235723/automated-20-04-server-installation-using-pxe-and-live-server-image
 
 apt-get -y install tftpd-hpa apache2
 
@@ -73,12 +76,12 @@ sudo umount ${TMP_PATH}
 
 sudo mkdir -p ${TFTP_ROOT}/grub
 echo "default=autoinstall
-timeout=30
+timeout=5
 timeout_style=menu
 menuentry \"Focal Live Installer - automate\" --id=autoinstall {
     echo \"Loading Kernel...\"
     # make sure to escape the ';'
-    linux /vmlinuz ip=dhcp url=http://\${pxe_default_server}/tftp/${UBUNTU_ISO} autoinstall ds=nocloud-net\;s=http://${pxe_default_server}/tftp/
+    linux /vmlinuz ip=dhcp url=http://\${pxe_default_server}/tftp/${UBUNTU_ISO} autoinstall ds=nocloud-net\;s=http://\${pxe_default_server}/tftp/
     echo \"Loading Ram Disk...\"
     initrd /initrd
 }
@@ -89,84 +92,22 @@ menuentry \"Focal Live Installer\" --id=install {
     initrd /initrd
 }" | sudo tee ${TFTP_ROOT}/grub/grub.cfg
 
-touch ${TFTP_ROOT}/meta-data
+sudo touch ${TFTP_ROOT}/meta-data
 #echo "instance-id: focal-autoinstall" | sudo tee ${TFTP_ROOT}/meta-data
 
 # https://ubuntu.com/server/docs/install/autoinstall
 
 echo "#cloud-config
 autoinstall:
-  version: 1
-  apt:
-    # even set to no/false, geoip lookup still happens
-    #geoip: no
-    preserve_sources_list: false
-    primary:
-    - arches: [amd64, i386]
-      uri: http://us.archive.ubuntu.com/ubuntu
-    - arches: [default]
-      uri: http://ports.ubuntu.com/ubuntu-ports
-  identity:
-    hostname: node
-    password: \$6\$ZoP3olp8Tg3ygmEm\$VFq3UDGbz6Sl.UCpEzZPicnYDaHn3lEqsjpwJwwKFNBlAcZpD5yns1uCI5fkfKjHDPkwGNEAEdbFjNoY2NjP/0
-    username: ubuntu
-  keyboard:
-    layout: us
-    variant: ''
+  identity: {hostname: node, password: \$6\$VzsMBQNG.jLDEFAp\$IseeLgTaGWnZaktBfN0RvoG7GSf7ra3rEo4FO4nXq.h25GoxLLHWZjfv/e8MBbojAzcKaagy2qAcApVKoFh3F/,
+    realname: ubuntu, username: ubuntu}
+  keyboard: {layout: us, toggle: null, variant: ''}
   locale: en_US
-  network:
-    network:
-      version: 2
-      ethernets:
-        ens192:
-          critical: true
-          dhcp-identifier: mac
-          dhcp4: true
-  user-data:
-    timezone: America/Los_Angeles
-    disable_root: false
-    chpasswd:
-      list: |
-        root:\$6\$ZoP3olp8Tg3ygmEm\$VFq3UDGbz6Sl.UCpEzZPicnYDaHn3lEqsjpwJwwKFNBlAcZpD5yns1uCI5fkfKjHDPkwGNEAEdbFjNoY2NjP/0
-  ssh:
-    allow-pw: true
-    authorized-keys:
-      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCquwVARyLFDuKZcuvpXZbJB/vlPz4yJpAjwsyltri40MoqAGwhSqz8kHycHBA7/wPDglIj6W6YGnvT3tXkyqhZD23zix6Q9RryCw3mVCjQzyMU0TbU3JkrJpgaekw+nmlRXY4DJ/4CdPnS3KdEvCYHEStFEaYutv0vNajqVNYlDFqo4/w60YRedd8eTf8AIbMurUcfgEmVk+lx0vVQhgMOzLHEUMhOeZEnPfheOX+/JGsuiAFiAwH6XVQuPmddNCjyxj7uC05SZQRt+KaeW6pVKjE2FAyNBpnVJpb1azWtPzs+BMLJbrctQd4NRkMCkPFDSW7O35nwDBsqgdt09XxM4kZlYCIUfZIf6Cm5Tyjrrv/ifgXnZikFIqieyFW34KaIvSmD9MGE4qhC1wR2sdTzaGYwrVy3UXc6u4ikrRj9TuZJWYHXJqw67EAgXw18YKszWRHEzlwALc32/jEplO0Ydfx+inauw8QNEXm+5Yce2nAGe93+Mbo6JWMZT4wviBR8LGlnhSLcUZIDRQo8oA+RVvZmvfn+XznITU6wA0armyswuWm1Vko5rkwISqIHQhLEkt6JCO8aX2YnsWlCb6bw+1E+OHH0rxhp2FIXeBk+12EfXNh7N/SYM5OVsyjvTbB/Otzl2qWFkL7ka15/ynXX75nlKnSIVrsiAjSAUy8GpQ== user
-    install-server: true
-  # this creates an efi partition, /boot partition, and root(/) lvm volume
-  storage:
-    grub:
-      reorder_uefi: False
-    swap:
-      size: 0
-    config:
-    - {ptable: gpt,
-      path: /dev/nvme0n1, wipe: superblock, preserve: false, name: '', grub_device: false,
-      type: disk, id: disk-nvme0n1}
-    - {device: disk-nvme0n1, size: 536870912, wipe: superblock, flag: boot, number: 1,
-      preserve: false, grub_device: true, type: partition, id: partition-0}
-    - {fstype: fat32, volume: partition-0, preserve: false, type: format, id: format-0}
-    - {device: disk-nvme0n1, size: 1073741824, wipe: superblock, flag: '', number: 2,
-      preserve: false, type: partition, id: partition-1}
-    - {fstype: ext4, volume: partition-1, preserve: false, type: format, id: format-1}
-    - {device: disk-nvme0n1, size: 248446451712, wipe: superblock, flag: '', number: 3,
-      preserve: false, type: partition, id: partition-2}
-    - name: ubuntu-vg
-      devices: [partition-2]
-      preserve: false
-      type: lvm_volgroup
-      id: lvm_volgroup-0
-    - {name: ubuntu-lv, volgroup: lvm_volgroup-0, size: 124222701568B, preserve: false,
-      type: lvm_partition, id: lvm_partition-0}
-    - {fstype: ext4, volume: lvm_partition-0, preserve: false, type: format, id: format-2}
-    - {device: format-2, path: /, type: mount, id: mount-2}
-    - {device: format-1, path: /boot, type: mount, id: mount-1}
-    - {device: format-0, path: /boot/efi, type: mount, id: mount-0}" | sudo tee ${TFTP_ROOT}/user-data
-
-
-
-
-
+  reporting:
+    builtin:
+      type: print  
+  ssh: {allow-pw: true, authorized-keys: ['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCquwVARyLFDuKZcuvpXZbJB/vlPz4yJpAjwsyltri40MoqAGwhSqz8kHycHBA7/wPDglIj6W6YGnvT3tXkyqhZD23zix6Q9RryCw3mVCjQzyMU0TbU3JkrJpgaekw+nmlRXY4DJ/4CdPnS3KdEvCYHEStFEaYutv0vNajqVNYlDFqo4/w60YRedd8eTf8AIbMurUcfgEmVk+lx0vVQhgMOzLHEUMhOeZEnPfheOX+/JGsuiAFiAwH6XVQuPmddNCjyxj7uC05SZQRt+KaeW6pVKjE2FAyNBpnVJpb1azWtPzs+BMLJbrctQd4NRkMCkPFDSW7O35nwDBsqgdt09XxM4kZlYCIUfZIf6Cm5Tyjrrv/ifgXnZikFIqieyFW34KaIvSmD9MGE4qhC1wR2sdTzaGYwrVy3UXc6u4ikrRj9TuZJWYHXJqw67EAgXw18YKszWRHEzlwALc32/jEplO0Ydfx+inauw8QNEXm+5Yce2nAGe93+Mbo6JWMZT4wviBR8LGlnhSLcUZIDRQo8oA+RVvZmvfn+XznITU6wA0armyswuWm1Vko5rkwISqIHQhLEkt6JCO8aX2YnsWlCb6bw+1E+OHH0rxhp2FIXeBk+12EfXNh7N/SYM5OVsyjvTbB/Otzl2qWFkL7ka15/ynXX75nlKnSIVrsiAjSAUy8GpQ== user'], install-server: true}
+  version: 1" | sudo tee ${TFTP_ROOT}/user-data
 
 
 
@@ -178,14 +119,18 @@ sudo apt-get update
 sudo apt-get -y install isc-dhcp-server
 
 # make the rpi the authoritative dhcp server
-sudo sed -i "/^#authoritative;/a\authoritative;" /etc/dhcp/dhcpd.conf
+#sudo sed -i "/^#authoritative;/a\authoritative;" /etc/dhcp/dhcpd.conf
 
-echo "subnet 10.10.0.0 netmask 255.255.255.0 {
+echo "authoritative
+subnet 10.10.0.0 netmask 255.255.255.0 {
   range 10.10.0.10 10.10.0.200;
   option routers 10.10.0.1;
-  option domain-name "bm";
-  option domain-name-servers 8.8.8.8, 8.8.4.4;
-}" | sudo tee -a /etc/dhcp/dhcpd.conf
+  option domain-name \"bm\";
+  option domain-name-servers 8.8.8.8, 192.168.86.1;
+}
+option tftp-server-name \"10.10.0.1\";
+option bootfile-name \"pxelinux.0\";
+" | sudo tee -a /etc/dhcp/dhcpd.conf
 
 # only do dhcp for ipv4 on eth0, leaving wlan0 alone
 sudo sed -i 's/INTERFACESv4=""/INTERFACESv4="eth0"/1 /etc/default/isc-dhcp-server
