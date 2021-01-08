0c080fee72d29b37b021d320c64534378a27aa21
Kent Hua <kenthua@google.com>
2021-01-08 21:24:01 +0000

    cleanup script

--- scripts/ubuntu/pxe.sh
+++ scripts/ubuntu/pxe.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# enable port forwarding
+# enable port forwarding and make it persistent
 sudo sysctl -w net.ipv4.ip_forward=1
-# persistent
 echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf
 
-# enable forwarding eth0 traffic through wlan0
+# enable forwarding eth0 traffic through wlan0 and make it persistent
 # https://serverfault.com/questions/152363/bridging-wlan0-to-eth0
 sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
-
 sudo apt-get install -y iptables-persistent
 
+# setup dns / tftp
 sudo apt-get -y install dnsmasq
 
 TFTP_ROOT=/var/lib/tftpboot
@@ -18,9 +17,8 @@ UBUNTU_ISO=ubuntu-20.04.1-live-server-amd64.iso
 TMP_PATH=/tmp/ubuntu
 
 ## modify resolv.conf accordingly
-# nameserver 192.168.86.1
+## /etc/resolv.conf
 # nameserver 10.10.0.1
-
 # options edns0
 # search anthos.cloud
 
@@ -28,43 +26,48 @@ TMP_PATH=/tmp/ubuntu
 sudo systemctl stop systemd-resolved
 sudo systemctl disable systemd-resolved
 
-sudo mkdir -p ${TFTP_ROOT}
 sudo cp dnsmasq.conf /etc/dnsmasq.conf
 sudo systemctl restart dnsmasq
 
+# setup apache
 sudo apt-get -y install apache2
 
+# copy / enable tftp configuration
 sudo cp tftp.conf /etc/apache2/conf-available/tftp.conf
-
 sudo a2enconf tftp
 
+# ensure apache is autostarted/config reloaded/restarted
 sudo systemctl enable apache2
 sudo systemctl reload apache2
-sudo systemctl restart apache2
 
+# grab the ubuntu iso
+sudo mkdir -p ${TFTP_ROOT}
 sudo curl -L https://releases.ubuntu.com/20.04/${UBUNTU_ISO} -o ${TFTP_ROOT}/${UBUNTU_ISO}
 sudo curl -L http://archive.ubuntu.com/ubuntu/dists/focal/main/uefi/grub2-amd64/current/grubnetx64.efi.signed -o ${TFTP_ROOT}/pxelinux.0
 
+# extract the boot files
 mkdir -p ${TMP_PATH}
 sudo mount ${TFTP_ROOT}/${UBUNTU_ISO} ${TMP_PATH}
 sudo cp ${TMP_PATH}/casper/vmlinuz ${TFTP_ROOT}
 sudo cp ${TMP_PATH}/casper/initrd ${TFTP_ROOT}
 sudo umount ${TMP_PATH}
 
+# prepare grub
 sudo mkdir -p ${TFTP_ROOT}/grub
 cat grub.cfg | sed "s/UBUNTU_ISO/${UBUNTU_ISO}/" | sudo tee ${TFTP_ROOT}/grub/grub.cfg
 
-sudo touch ${TFTP_ROOT}/meta-data
-
 # https://ubuntu.com/server/docs/install/autoinstall
-
+# prepare ubuntu auto installer
+sudo touch ${TFTP_ROOT}/meta-data
 sudo cp user-data ${TFTP_ROOT}/user-data
 
+# get ansible ready
 sudo apt update
 sudo apt install ansible
 
-
-
+#####################
+## cleaning the disk
+#####################
 # wipe disk fdisk - fast just wipe parition table
 echo "d
 3
@@ -76,23 +79,6 @@ w" | sudo fdisk /dev/nvme0n1
 
 # wipe disk dd -- ctrl-c after a few sec
 sudo dd if=/dev/zero of=/dev/nvme0n1 bs=1M
-
-
-
-network:
-  version: 2
-  renderer: networkd
-  ethernets:
-    enp3s0:
-      addresses:
-        - 10.10.0.1/24
-      nameservers:
-          addresses: [8.8.8.8, 192.168.1.1]
-
-
-
-enp1s0
-
 
 
 pi@rpi:~/anthos-bm-build/ansible $ cat wakeup.yaml
