383a379a74765bf2d4082ea8b7cf22e86b5d77d9
Kent Hua <pitchdarkice@gmail.com>
2021-01-22 00:16:09 -0500

    cleanup

--- bm/foundation/ksa-binding.sh
+++
@@ -1,9 +0,0 @@
-KSA_NAME=console
-kubectl create serviceaccount ${KSA_NAME}
-kubectl create clusterrolebinding console-view-binding \
-  --clusterrole view --serviceaccount default:${KSA_NAME}
-kubectl create clusterrolebinding console-reader-binding \
-  --clusterrole cloud-console-reader --serviceaccount default:${KSA_NAME}
-
-SECRET_NAME=$(kubectl get serviceaccount console -o jsonpath='{$.secrets[0].name}')
-kubectl get secret ${SECRET_NAME} -o jsonpath='{$.data.token}' | base64 --decode
